7c7ceff769335dabe52aedb33ce2e05e27562523
Daria <dariamirestean@gmail.com>
2023-10-29 19:58:48 +0200

    Faza 2 a proiectului

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string.h>
+#include <cstring>
 using namespace std;
 
 // am ales domeniul MeDical cu obiectele medicamente, pacienti, aparatura medicala
 
 class Medicament{
-public:
+private:
     string nume;
     float pretCutie; 
     const int id; // atribut constant
     static int contor; // atribut static, contor al medicamentelor administrate per total
     int nrIngrediente;
     string* ingredientePrincipale;
+
+public:
+    // declaratia de prietenie a functiei globale de modificare a pretului
+    friend void modificaPretMedicament(Medicament& medicament, float pretNou);
 
     // Constructor fara parametri
     Medicament(): id(0){
@@ -54,19 +59,69 @@ public:
     static double calculareDozaRecomandata(double greutate, double concentratie){
         return greutate * concentratie;
     }
-    
-    // Destructorul 
+
+     // Constructor de copiere
+    Medicament(const Medicament& medicament) : id(contor) {
+        nume = medicament.nume;
+        pretCutie = medicament.pretCutie;
+        nrIngrediente = medicament.nrIngrediente;
+        ingredientePrincipale = new string[nrIngrediente];
+        for (int i = 0; i < nrIngrediente; i++) {
+            ingredientePrincipale[i] = medicament.ingredientePrincipale[i];
+        }
+        contor++;
+    }
+
+    // Metode de acces
+    string getNume() const {
+        return nume;
+    }
+
+    float getPretCutie() const {
+        return pretCutie;
+    }
+
+    int getId() const {
+        return id;
+    }
+
+    int getNrIngrediente() const {
+        return nrIngrediente;
+    }
+
+    string* getIngredientePrincipale() const {
+        return ingredientePrincipale;
+    }
+
+    static int getContor() {
+        return contor;
+    }
+
+    // Metode de modificare
+    void setNume(const string& nume) {
+        this->nume = nume;
+    }
+
+    void setPretCutie(float pretCutie) {
+        this->pretCutie = pretCutie;
+    }
+
+    // destructorul 
     ~Medicament(){
-        if (this->ingredientePrincipale != NULL) {
-            delete[]this->ingredientePrincipale;
-            this->ingredientePrincipale = NULL;
+        if (ingredientePrincipale != nullptr) {
+            delete[] ingredientePrincipale;
+            ingredientePrincipale = nullptr;
         }
     }
 };
 int Medicament::contor = 0;
+// functia prietena cu clasa Medicament
+void modificaPretMedicament(Medicament& medicament, float pretNou) {
+    medicament.setPretCutie(pretNou);
+}
 
 class Pacient{
-public:
+private:
     string nume;
     int varsta;
     string nrTelefon;
@@ -75,6 +130,7 @@ public:
     int nrAfectiuni;
     string* afectiuni;
 
+public: 
     // Constructor fara parametri
     Pacient(): CNP(7394826157){
         this->nume = "Ion Popescu";
@@ -107,6 +163,57 @@ public:
         afectiuni[2] = "Hipotensiune";
         this->contorFisePacienti++;
     }
+    
+     // Constructor de copiere
+    Pacient(const Pacient& pacient) : CNP(pacient.CNP) {
+        nume = pacient.nume;
+        varsta = pacient.varsta;
+        nrTelefon = pacient.nrTelefon;
+        nrAfectiuni = pacient.nrAfectiuni;
+        afectiuni = new string[nrAfectiuni];
+        for (int i = 0; i < nrAfectiuni; i++) {
+            afectiuni[i] = pacient.afectiuni[i];
+        }
+        contorFisePacienti++;
+    }
+
+    // Metode de acces
+    string getNume() const {
+        return nume;
+    }
+
+    int getVarsta() const {
+        return varsta;
+    }
+
+    string getNrTelefon() const {
+        return nrTelefon;
+    }
+
+    long getCNP() const {
+        return CNP;
+    }
+
+    int getNrAfectiuni() const {
+        return nrAfectiuni;
+    }
+
+    string* getAfectiuni() const {
+        return afectiuni;
+    }
+
+    // Metode de modificare
+    void setNume(const string& nume) {
+        this->nume = nume;
+    }
+
+    void setVarsta(int varsta) {
+        this->varsta = varsta;
+    }
+
+    void setNrTelefon(const string& nrTelefon) {
+        this->nrTelefon = nrTelefon;
+    }
 
     void afisare() {
         cout << "Pacient: " << this->nume << "\nVarsta: " << this->varsta << " ani\nCNP: " << this->CNP << "\nNr. telefon: " << this->nrTelefon << "\nAcesta sufera de " << this->nrAfectiuni << "afectiuni:\n";
@@ -122,16 +229,16 @@ public:
 
     // destructorul
     ~Pacient(){
-        if (this->afectiuni != NULL) {
-            delete[]this->afectiuni;
-            this->afectiuni = NULL;
+        if (afectiuni != NULL) {
+            delete[] afectiuni;
+            afectiuni = NULL;
         }
     }
 };
 int Pacient::contorFisePacienti = 0;
 
 class Aparatura{
-public: 
+private: 
     string denumire;
     int pret;
     string sectie;
@@ -139,6 +246,11 @@ public:
     const string marca;
     int nrMediciCareFolosescAparatul;
     string* numeMediciCareFolosescAparatul;
+
+
+public:
+    // declaratia de prietenie a functiei globalr
+    friend void afisareAparatura(const Aparatura& aparat);
 
     //Constructor fara parametri
     Aparatura(): marca("Comen"){
@@ -170,27 +282,80 @@ public:
         numeMediciCareFolosescAparatul[2] = "Dr. Alisa Marinescu";
     }
 
-    void afisare() {
-        cout << "Denumire aparat: " << this->denumire << "\nPret: " << this->pret << "\nSectie: " << this->sectie << "\nTVA: " << this->TVA << "\nMarca: " << this->marca << "\n";
-        cout << "Medicii care pot folosi aparatul sunt urmatorii: ";
-        for (int i = 0; i < this->nrMediciCareFolosescAparatul - 1; i++)
-            cout << this->numeMediciCareFolosescAparatul[i] << ", ";
-        cout << this->numeMediciCareFolosescAparatul[nrMediciCareFolosescAparatul - 1] << ".\n";
+    static void setTVA(float TVAnou) { 
+        TVA = TVAnou;
+    }
+    
+    // Constructor de copiere
+    Aparatura(const Aparatura& aparatura) : marca(aparatura.marca) {
+        denumire = aparatura.denumire;
+        pret = aparatura.pret;
+        sectie = aparatura.sectie;
+        TVA = aparatura.TVA;
+        nrMediciCareFolosescAparatul = aparatura.nrMediciCareFolosescAparatul;
+        numeMediciCareFolosescAparatul = new string[nrMediciCareFolosescAparatul];
+        for (int i = 0; i < nrMediciCareFolosescAparatul; i++) {
+            numeMediciCareFolosescAparatul[i] = aparatura.numeMediciCareFolosescAparatul[i];
+        }
     }
 
-static void setTVA(float TVAnou) { 
-    TVA = TVAnou;
-}
-
-// Destructorul
-~Aparatura(){
-    if (this->numeMediciCareFolosescAparatul != NULL) {
-        delete[] this->numeMediciCareFolosescAparatul;
-        this->numeMediciCareFolosescAparatul = NULL;
+    // metode de acces
+    string getDenumire() const {
+        return denumire;
     }
-}
+
+    int getPret() const {
+        return pret;
+    }
+
+    string getSectie() const {
+        return sectie;
+    }
+
+    static float getTVA() {
+        return TVA;
+    }
+
+    string getMarca() const {
+        return marca;
+    }
+
+    int getNrMediciCareFolosescAparatul() const {
+        return nrMediciCareFolosescAparatul;
+    }
+
+    string* getNumeMediciCareFolosescAparatul() const {
+        return numeMediciCareFolosescAparatul;
+    }
+
+    // metode de modificare
+    void setDenumire(const string& denumire) {
+        this->denumire = denumire;
+    }
+
+    void setPret(int pret) {
+        this->pret = pret;
+    }
+
+    void setSectie(const string& sectie) {
+        this->sectie = sectie;
+    }
+    // destructorul
+    ~Aparatura() {
+        if (numeMediciCareFolosescAparatul != NULL){
+            delete[] numeMediciCareFolosescAparatul;
+            numeMediciCareFolosescAparatul = NULL;
+        }
+    }
 };
 float Aparatura::TVA = 0.19;
+// functia prietena cu clasa Aparatura
+void afisareAparatura(const Aparatura& aparat) {
+    cout << "Denumire aparat: " << aparat.denumire << "\nPret: " << aparat.pret << "\nSectie: " << aparat.sectie << "\nTVA: " << aparat.TVA << "\nMarca: " << aparat.marca << "\nMedicii care pot folosi aparatul sunt urmatorii: ";
+    for (int i = 0; i < aparat.nrMediciCareFolosescAparatul - 1; i++)
+        cout <<aparat.numeMediciCareFolosescAparatul[i] << ", ";
+    cout << aparat.numeMediciCareFolosescAparatul[aparat.nrMediciCareFolosescAparatul - 1] << ".\n";
+}
 
 int main() {
     // Pentru Medicament
@@ -203,6 +368,7 @@ int main() {
     Medicament medicament3("Aerius", 20.0, ingrediente3); // Constructor cu 3 parametri
 
     // afisez cele 3 obiecte Medicament + atributele lor
+    modificaPretMedicament(medicament1, 12.5);
     medicament1.afisare();
     cout<<"\n";
 
@@ -216,9 +382,9 @@ int main() {
     double greutatePacient = 52; // Greutatea pacientului în kg
     double concentratieMedicament3 = 0.5; // Concentrația medicamentului 3
 
-    double dozaRecomandata = Medicament::calculareDozaRecomandata(greutatePacient, concentratieMedicament3);
-    cout << "Doza recomandata de " << medicament3.nume << " pentru un pacient cu greutatea de " << greutatePacient << " kg si o concentratie de " << concentratieMedicament3 << " este " << dozaRecomandata << " grame.\n\n";
-    } 
+   double dozaRecomandata = Medicament::calculareDozaRecomandata(greutatePacient, concentratieMedicament3);
+   cout << "Doza recomandata de " << medicament3.getNume() << " pentru un pacient cu greutatea de " << greutatePacient << " kg si o concentratie de " << concentratieMedicament3 << " este " << dozaRecomandata << " grame.\n\n";
+    }
 
     // Pentru Pacient
     {
@@ -248,15 +414,13 @@ int main() {
         Aparatura aparat3("Computer Tomograf", 40000);
 
     // afisez aparatele
-    aparat1.afisare();
+    afisareAparatura(aparat1);
     cout<<"\n";
-
-    aparat2.afisare();
+    afisareAparatura(aparat2);
     cout<<"\n";
-
     Aparatura::setTVA(0.21); // aici am folosit functia statica, crescand TVA-ul
-    aparat3.afisare();
-    cout<<"\n";
+    afisareAparatura(aparat3);
+
    }
    
    return 0;
